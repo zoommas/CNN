aeaf41439ccb87d973a139cb990f7bb79ef374d3
zoommas <yalehanr@gmail.com>
2025-06-09 18:05:54 -0400

    created model with tensorflow 2.x

---
+++ Model.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+
+from keras import layers, models
+
+class MyModel(tf.keras.Model):
+    def __init__(self, num_labels=10):
+        super(MyModel, self).__init__()
+        self.conv1 = layers.Conv2D(32, (5, 5), activation='relu', padding='same')
+        self.pool1 = layers.MaxPool2D((2, 2))
+
+        self.conv2 = layers.Conv2D(64, (5, 5), activation='relu', padding='same')
+        self.pool2 = layers.MaxPool2D((2, 2))
+
+        self.flatten = layers.Flatten()
+        self.fc1 = layers.Dense(1024, 'relu')
+        self.dropout = layers.Dropout(0.5)
+        self.fc2 = layers.Dense(num_labels)
+
+    def call(self, input, training):
+        input = self.conv1(input)
+        input = self.pool1(input)
+        input = self.conv2(input)
+        input = self.pool2(input)
+        input = self.flatten(input)
+        input = self.fc1(input)
+        if training:
+            input = self.dropout(input)
+        input = self.fc2(input)
+        return input
+    
+
