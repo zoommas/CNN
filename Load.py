e21d09f25fc0563ed1924644c87ff159486d2e44
zoommas <yalehanr@gmail.com>
2025-06-10 16:01:01 -0400

    Working with Tensorflow 2.x

--- Load.py
+++ Load.py
@@ -3,45 +3,23 @@ import numpy as np
 IMAGE_SIZE = 28
 
 """
-Loads in MNIST training dataset. Splits dataset into validation set and training set
+Loads in MNIST training dataset. Splits dataset into training set
 :return: 3D Tensor input of training/validation set and one hot encoded image labels
 
 """
 
-def load_training_data(data_path, validation_size = 500):
+def load_data(data_path):
     
 
 
     training_data = np.genfromtxt(data_path, delimiter=',', dtype=np.float32)
+    training_data = training_data[~np.isnan(training_data).any(axis=1)]
     x_train = training_data[:, 1:]
     y_train = training_data[:, 0]
-    y_train = (np.arange(10) == y_train[:, None].astype(np.float32))
-
-    x_train, x_val, y_train, y_val = x_train[0:(len(x_train) - validation_size), :], x_train[len(
-        x_train) - validation_size:len(x_train), :], \
-                                     y_train[0:(len(y_train) - validation_size), :], y_train[len(
-        y_train) - validation_size: len(y_train), :]
-    
+    y_train = (np.arange(10) == y_train[:, None]).astype(np.float32)
     x_train = x_train.reshape(len(x_train), IMAGE_SIZE, IMAGE_SIZE, 1)
-    x_val = x_val.reshape(len(x_val), IMAGE_SIZE, IMAGE_SIZE, 1)
+    x_train = x_train.astype(np.float32) / 255.0
 
-    return x_train, x_val, y_train, y_val
-
-"""
-Loads in MNIST test dataset.
-:return: 3D Tensor input of test set and one hot encoded image labels
-"""
-
-def load_test_data(data_path):
-    
-    test_data = np.genfromtxt(data_path, delimiter=',', dtype=np.float32)
-    x_test = test_data[:, 1:]
-    y_test = test_data[:, 0]
-    y_test = (np.arange(10) == y_test[:, None].astype(np.float32))
-    x_test = x_test.reshape(len(x_test), IMAGE_SIZE, IMAGE_SIZE, 1)
-
-    return x_test, y_test
-
-
+    return x_train, y_train
 
 
--- Model.py
+++ Model.py
@@ -1,6 +1,10 @@
+from matplotlib import pyplot as plt
 import tensorflow as tf
+import numpy as np
 
 from keras import layers, models
+
+import Load
 
 class MyModel(tf.keras.Model):
     def __init__(self, num_labels=10):
@@ -16,7 +20,7 @@ class MyModel(tf.keras.Model):
         self.dropout = layers.Dropout(0.5)
         self.fc2 = layers.Dense(num_labels)
 
-    def call(self, input, training):
+    def call(self, input, training=False):
         input = self.conv1(input)
         input = self.pool1(input)
         input = self.conv2(input)
@@ -28,4 +32,28 @@ class MyModel(tf.keras.Model):
         input = self.fc2(input)
         return input
     
+def main():
+    x_train, y_train = Load.load_data('data/mnist_train.csv')
+    x_test, y_test = Load.load_data('data/mnist_test.csv')
+    model = MyModel(num_labels=10)
+    optimizer = tf.keras.optimizers.Adam(learning_rate=1e-5)
+    model.compile(optimizer=optimizer,
+              loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+    history = model.fit(x_train, y_train, epochs=10, 
+                    validation_split=0.1)
+    model.evaluate(x_test, y_test)
+    plt.plot(history.history['accuracy'], label='accuracy')
+    plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.ylim([0.5, 1])
+    plt.legend(loc='lower right')
+    plt.savefig("training_plot.png")
+    plt.show()
 
+    test_loss, test_acc = model.evaluate(x_test,  y_test, verbose=2)
+    print(test_acc)
+
+if __name__ == '__main__':
+    main()
